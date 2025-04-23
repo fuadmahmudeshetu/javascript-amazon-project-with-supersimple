17c999af9ba618d2cb47cbf5c910daba165f7eb1
Fuad Mahmud <fuadmahmudeshetu@gmail.com>
2025-04-23 23:02:45 +0300

    Make add to cart interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -18,12 +18,12 @@ products.forEach((product)=>{
                 <img class="product-rating-stars"
                 src="images/ratings/rating-${product.rating.stars*10}.png">
                 <div class="product-rating-count link-primary">
-                ${product.rating.count}
+                ${product.rating.count / 10}
                 </div>
             </div>
 
             <div class="product-price">
-                $${(product.priceCent/100).toFixed(2)}
+                $${(product.priceCents/100).toFixed(2)}
             </div>
 
             <div class="product-quantity-container">
@@ -48,17 +48,36 @@ products.forEach((product)=>{
                 Added
             </div>
 
-            <button class="add-to-cart-button button-primary js-add-to-cart">
+            <button class="add-to-cart-button button-primary js-add-to-cart
+            " data-product-id="${product.id}">
                 Add to Cart
             </button>
-        </div>`;
-    }
-);
+        </div>`
+        ;
+});
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
 document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
     button.addEventListener('click',()=>{
-        console.log('Product Added');
-    })
-})
+        const productId = button.dataset.productId;
+
+        let matchingItem;
+
+        cart.forEach((item)=>{
+            if(productId === item.productId){
+                matchingItem = item;
+            }
+        });
+
+        if (matchingItem){
+            matchingItem.quantity += 1;
+        } else {
+            cart.push({
+                productId: productId,
+                quantity: 1
+            });
+        }
+        console.log(cart);
+    });
+});
